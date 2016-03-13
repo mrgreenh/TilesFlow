0c3607eaa78e8c6fab210efe6b1714f6d5814cfc
Carlo Andrea Conte <carloandreaconte@icloud.com>
2016-03-12 18:19:27 -0800

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+require('normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+let yeomanImage = require('../images/yeoman.png');
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div className="index">
+        <img src={yeomanImage} alt="Yeoman Generator" />
+        <div className="notice">Yo!</div>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
