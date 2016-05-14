6c3b9e536a7877d2b71cbc9ebda34afd4e3526b6
Carlo Andrea Conte <carloandreaconte@icloud.com>
2016-05-13 23:23:11 -0700

    Trying to put it together as a library

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,32 @@
-'use strict';
+var Webpack = require('webpack');
+var path = require('path');
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+var ForceCaseSensitivityPlugin = require('force-case-sensitivity-webpack-plugin');
 
-const path = require('path');
-const args = require('minimist')(process.argv.slice(2));
+var config = {
 
-// List of allowed environments
-const allowedEnvs = ['dev', 'dist', 'test'];
-
-// Set the correct environment
-var env;
-if(args._.length > 0 && args._.indexOf('start') !== -1) {
-  env = 'test';
-} else if (args.env) {
-  env = args.env;
-} else {
-  env = 'dev';
-}
-process.env.REACT_WEBPACK_ENV = env;
-
-// Get available configurations
-const configs = {
-  base: require(path.join(__dirname, 'cfg/base')),
-  dev: require(path.join(__dirname, 'cfg/dev')),
-  dist: require(path.join(__dirname, 'cfg/dist')),
-  test: require(path.join(__dirname, 'cfg/test'))
+    // We change to normal source mapping
+    devtool: 'source-map',
+    entry: './src/TilesFlow.js',
+    output: {
+        filename: './dist/bundle.js',
+        library: "tilesflow",
+        libraryTarget: "umd"
+    },
+    module: {
+        loaders: [{
+                test: /\.(js|jsx)$/,
+                loader: 'babel',
+                exclude: /(node_modules)/,
+                query: {
+                    presets: ["es2015", "stage-1"],
+                    plugins: ['transform-runtime']
+                }
+            }]
+    },
+    plugins: [
+        new ForceCaseSensitivityPlugin()
+    ]
 };
 
-/**
- * Build the webpack configuration
- * @param  {String} wantedEnv The wanted environment
- * @return {Object} Webpack config
- */
-function buildConfig(wantedEnv) {
-  let isValid = wantedEnv && wantedEnv.length > 0 && allowedEnvs.indexOf(wantedEnv) !== -1;
-  let validEnv = isValid ? wantedEnv : 'dev';
-  return configs[validEnv];
-}
-
-module.exports = buildConfig(env);
+module.exports = config;
